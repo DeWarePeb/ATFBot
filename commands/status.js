ff0cf5a69b0bd67a8fcd9a30d560879b8d0baa89
DeWarePeb <68034923+DeWarePeb@users.noreply.github.com>
2020-07-10 00:36:38 +0200

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -3,7 +3,7 @@ const client = new discord.Client();
 
 module.exports.run = async(client, message, args) =>{
     //OWNER ONLY COMMAND
-if(!message.author.id === "My id") {
+if(!message.author.id === "271350440522809356") {
     return message.channel.send("This command can only be used by owner")
   }
   //ARGUMENT
