67c146b67ca64ec7ddea6a92812c69935b45210b
DeWarePeb <68034923+DeWarePeb@users.noreply.github.com>
2020-07-09 22:00:44 +0200

    init

---
+++ commands/pokemon.js
@@ -0,0 +1,14 @@
+const discord = require("discord.js");
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
+
+module.exports.run = async(bot, message, args) =>{
+    let response = await fetch(`${BASE_URL}/${pokemon}`);
+    return await response.json();
+    
+}
+
+module.exports.help = {
+    name: "pokemon",
+    description: "Hi",
+    category: "Fun"
+}
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const botconfig = require("./botconfig.json");
 const fs = require("fs");
 const warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
-
+const { getPokemon } = require('./commands/pokemon');
 
 const client = new discord.Client();
 client.commands = new discord.Collection();
@@ -99,6 +99,41 @@ client.on("guildMemberRemove", member => {
     
 
 })
+
+//pokemon
+client.on('message', async message => {
+    if(message.author.bot) return;
+    if(message.content.toLowerCase().startsWith('!pokemon')) {
+        const pokemon = message.content.toLowerCase().split(" ")[1];
+        try {
+            const pokeData = await getPokemon(pokemon);
+            const { 
+                sprites, 
+                stats, 
+                weight, 
+                name, 
+                id, 
+                base_experience,
+                abilities,
+                types
+            } = pokeData;
+            const embed = new MessageEmbed();
+            embed.setTitle(`${name} #${id}`)
+            embed.setThumbnail(`${sprites.front_default}`);
+            stats.forEach(stat => embed.addField(stat.stat.name, stat.base_stat, true));
+            types.forEach(type => embed.addField('Type', type.type.name, true));
+            embed.addField('Weight', weight);
+            embed.addField('Base Experience', base_experience);
+            message.channel.send(embed);
+        }
+        catch(err) {
+            console.log(err);
+            message.channel.send(`Pokemon ${pokemon} does not exist.`);
+        }
+    }
+});
+
+//pokemon
 
 
 client.on("message", async message => {
