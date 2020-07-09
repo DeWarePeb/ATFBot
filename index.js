bf57a8261e6695d4e73ef6634515acd3dfe89c58
DeWarePeb <68034923+DeWarePeb@users.noreply.github.com>
2020-07-10 01:23:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -54,6 +54,24 @@ client.on('messageDelete', message => {
         }
     }
 });
+
+client.on('messageUpdate', message => {
+    try {
+        var embed = new discord.MessageEmbed()
+          .setTitle(`New message edited`)
+          .setColor(`GREEN`)
+          .setDescription(
+            `**The user ${oldMessage.author.tag} has edited a message in <#${oldMessage.channel.id}>**`
+          )
+          .addField(`Old Content`, oldMessage.content, true)
+          .addField(`New Content`, newMessage.content, true);
+        var channel = oldMessage.guild.channels.cache.get;(
+          (ch) => ch.name === "🔫server-log"
+        );
+        if (!channel) return;
+        channel.send(embed);
+      } catch (e) {}
+});
 //log
 
 client.on("guildMemberAdd", member => {
