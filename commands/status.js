9820e776f9163ab649f2bbdc8d4c56c8751a52db
DeWarePeb <68034923+DeWarePeb@users.noreply.github.com>
2020-07-10 00:47:25 +0200

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -3,8 +3,8 @@ const client = new discord.Client();
 
 module.exports.run = async(client, message, args) =>{
     //OWNER ONLY COMMAND
-if(!message.author.id === "271350440522809356") {
-    return message.channel.send("This command can only be used by owner")
+  if (!message.member.hasPermission("KICK_MEMBERS")) {
+    return message.channel.send("This command can only be used by big boys")
   }
   //ARGUMENT
   if(!args.length) {
