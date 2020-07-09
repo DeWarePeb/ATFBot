951ea0b17b7dab3d9c377583e98e27243a0f27f2
DeWarePeb <68034923+DeWarePeb@users.noreply.github.com>
2020-07-09 14:23:06 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
         .setColor("#0099ff")
         .setDescription(`**Fun commands** \n /rps - Rock Paper Sissors command \n /hi - says hi \n\n **Info commands** \n /avatar - gives the avatar of a member \n /info - Gives information about the bot \n /logo - gives the crew logo \n /rules - gives the crew rules \n /server-info - gives information about the server \n /suggestion - make a suggestion \n /ticket - open a ticket \n\n **Moderation commands** \n /ban - ban someone \n /clear - delete messages from a channel \n /kick - kick someone \n /mute - mute someone \n /tempmute - temp mute someone \n /unmute - unmute someone `)
         .setTimestamp()
-        .addFooter("Bot Name", client.user.username)
+        .setFooter("Bot Name", client.user.username)
     return message.channel.send(helpEmbed);
 
 
