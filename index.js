41a68a109c1851b30b9b9eecd2b75c1b1f9f9b57
DeWarePeb <68034923+DeWarePeb@users.noreply.github.com>
2020-07-09 21:46:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -45,8 +45,8 @@ client.on('messageDelete', message => {
         if(channel) {
             var embed = new discord.MessageEmbed()
                 .setTitle('Deleted Message')
-                .addField('Author', `${message.author.tag} (${message.author.id})`, true)
-                .addField('Channel', `${message.channel.name} (${message.channel.id})`, true)
+                .addField('Author', `@${message.author.tag}`, true)
+                .addField('Channel', `#${message.channel.name}`, true)
                 .setDescription(message.content)
                 .setTimestamp();
             channel.send(embed);
