84091672b4cf4ab038a918dbefa004efbbb2eef5
DeWarePeb <68034923+DeWarePeb@users.noreply.github.com>
2020-07-10 01:29:05 +0200

    c

--- commands/log.js
+++ commands/log.js
@@ -1,23 +1,24 @@
 const discord = require("discord.js");
 
-module.exports.run = async (oldMessage, newMessage) =>{
-    try {
-        var embed = new discord.MessageEmbed()
-          .setTitle(`New message edited`)
-          .setColor(`GREEN`)
-          .setDescription(
-            `**The user ${oldMessage.author.tag} has edited a message in <#${oldMessage.channel.id}>**`
-          )
-          .addField(`Old Content`, oldMessage.content, true)
-          .addField(`New Content`, newMessage.content, true);
-        var channel = oldMessage.guild.channels.cache.get;(
-          (ch) => ch.name === "🔫server-log"
-        );
-        if (!channel) return;
-        channel.send(embed);
-      } catch (e) {}
+const { MessageEmbed } = require("discord.js");
+module.exports = async (oldMessage, newMessage) => {
+  try {
+    let embed = new MessageEmbed()
+      .setTitle(`New message edited`)
+      .setColor(`GREEN`)
+      .setDescription(
+        `**The user ${oldMessage.author.tag} has edited a message in <#${oldMessage.channel.id}>**`
+      )
+      .addField(`Old Content`, oldMessage.content, true)
+      .addField(`New Content`, newMessage.content, true);
+    let channel = oldMessage.guild.channels.cache.find(
+      (ch) => ch.name === "🔫server-log"
+    );
+    if (!channel) return;
+    channel.send(embed);
+  } catch (e) {}
+};
 
-}
 
 module.exports.help = {
     name: "log",
