4bfe316b67862f7ce0a49f744db946c9d2a76c43
DeWarePeb <68034923+DeWarePeb@users.noreply.github.com>
2020-07-09 23:40:37 +0200

    Create status.js

---
+++ commands/status.js
@@ -0,0 +1,24 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) =>{
+    //OWNER ONLY COMMAND
+if(!message.author.id === "YOUR ID") {
+    return message.channel.send("This command can only be used by owner")
+  }
+  //ARGUMENT
+  if(!args.length) {
+    return message.channel.send("Please give status message")
+  }
+  
+db.set(`status`, args.join(" "))
+ await message.channel.send("Updated the bot status")
+  process.exit(1);
+    
+
+}
+
+module.exports.help = {
+    name: "hi",
+    description: "Hi",
+    category: "Fun"
+}
