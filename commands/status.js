10837769576d26ec700dc5ac70f84176d218a753
DeWarePeb <68034923+DeWarePeb@users.noreply.github.com>
2020-07-10 00:32:46 +0200

    Update status.js

--- commands/status.js
+++ commands/status.js
@@ -11,7 +11,7 @@ if(!message.author.id === "My id") {
     return message.channel.send("Please give status message")
   }
   
-  client.user.setStatus(`status`, args.join(" "))
+  client.user.setPresence({ activity: { name: args.join(" ") }})
  await message.channel.send("Updated the bot status")
   process.exit(1);
     
