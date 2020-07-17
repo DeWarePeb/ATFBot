931c867016a4206565c3d5270bd970a266038012
DeWarePeb <68034923+DeWarePeb@users.noreply.github.com>
2020-07-18 01:47:39 +0200

    cat

---
+++ commands/randomcat.js
@@ -0,0 +1,30 @@
+const discord = require("discord.js");
+
+module.exports.run = async(client, message, args) =>{
+    const SA = require('superagent')
+    try {
+      const res = await SA.get('https://aws.random.cat/meow')
+      const fact = (await SA.get('https://catfact.ninja/fact')).body.fact
+      await this.safeSendMessage(msg.channel, {
+        embed: {
+          description: fact,
+          image: {
+            url: res.body.file
+          },
+          footer: {
+            text: 'random.cat'
+          }
+        }
+      })
+    } catch (e) {
+      this.safeSendMessage(msg.channel, 'Something went wrong, try again later')
+      logger.error('CMD', e)
+    }
+    
+}
+
+module.exports.help = {
+    name: "say",
+    description: "Hi",
+    category: "Fun"
+}
