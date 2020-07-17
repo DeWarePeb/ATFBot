e5cab93edabf8170d4a7692a554abeb6fd130937
DeWarePeb <68034923+DeWarePeb@users.noreply.github.com>
2020-07-18 01:19:37 +0200

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -1,12 +1,8 @@
 const discord = require("discord.js");
 
 module.exports.run = async(client, message, args) =>{
-    let MSG = message.content.split(`${client.prefix}say `).join("");
-    if (!MSG)
-      return message.channel.send(`You did not specify your message to send!`);
-    message.channel.send(MSG);
-    message.delete();
-
+    return message.channel.send(`${args}`);
+    
 }
 
 module.exports.help = {
