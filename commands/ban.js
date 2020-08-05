6212763e9a32adab10aac334e0365cc06c55725b
DeWarePeb <68034923+DeWarePeb@users.noreply.github.com>
2020-08-05 23:13:36 +0200

    f

--- commands/ban.js
+++ commands/ban.js
@@ -27,72 +27,14 @@ module.exports.run = async (bot, message, args) => {
         **Banned By:** ${message.author}
         **Reason: ** ${reason}`);
 
-    var embedPrompt = new discord.MessageEmbed()
-        .setColor("GREEN")
-        .setAuthor("React whitin 30 sec.")
-        .setDescription(`Do you want to ban ${banUser}?`);
-
-
-    message.channel.send(embedPrompt).then(async msg => {
-
-        var emoji = await promptMessage(msg, message.author, 30, ["✅", "❌"]);
-
-
-        // We kijken dat het de gebruiker is die het als eerste heeft uitgevoerd.
-        // message.channel.awaitMessages(m => m.author.id == message.author.id,
-        //     { max: 1, time: 30000 }).then(collected => {
-
-        //         if (collected.first().content.toLowerCase() == 'yes') {
-        //             message.reply('Kick speler.');
-        //         }
-        //         else
-        //             message.reply('Geanuleerd');
-
-        //     }).catch(() => {
-        //         message.reply('Geen antwoord na 30 sec, geanuleerd.');
-        //     });
-
-
-        if (emoji === "✅") {
-
-            msg.delete();
-
-
-            banUser.ban(reason).catch(err => {
-                if (err) return message.channel.send(`Something went wrong`);
-            });
-
-            message.reply(embed);
-
-        } else if (emoji === "❌") {
-
-            msg.delete();
-
-            message.reply("Ban cancceled").then(m => m.delete(5000));
-
-        }
-
+    banUser.ban(reason).catch(err => {
+        if (err) return message.channel.send(`Something went wrong`);
     });
+
+    message.reply(embed);
+
 }
 
-// Emojis aan teksten kopellen.
-async function promptMessage(message, author, time, reactions) {
-    // We gaan eerst de tijd * 1000 doen zodat we seconden uitkomen.
-    time *= 1000;
-
-    // We gaan ieder meegegeven reactie onder de reactie plaatsen.
-    for (const reaction of reactions) {
-        await message.react(reaction);
-    }
-
-    // Als de emoji de juiste emoji is die men heeft opgegeven en als ook de auteur die dit heeft aangemaakt er op klikt
-    // dan kunnen we een bericht terug sturen.
-    const filter = (reaction, user) => reactions.includes(reaction.emoji.name) && user.id === author.id;
-
-    // We kijken als de reactie juist is, dus met die filter en ook het aantal keren en binnen de tijd.
-    // Dan kunnen we bericht terug sturen met dat icoontje dat is aangeduid.
-    return message.awaitReactions(filter, { max: 1, time: time }).then(collected => collected.first() && collected.first().emoji.name);
-}
 module.exports.help = {
     name: "ban",
     description: "Bans",
